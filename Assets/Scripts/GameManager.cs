9b5c808b73d0c5fb053cbce4cfd3f35f0745fabc
basarab1504 <45500276+basarab1504@users.noreply.github.com>
2021-04-01 16:23:36 +0300

    spawnManager deleted

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,13 +5,17 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] private List<Flask> flasks;
     [SerializeField] private List<Color> colors;
+    [SerializeField] private Ball prefab;
+    [SerializeField] private int flaskCapacity;
+
     private Ball taken;
 
     public IReadOnlyList<Flask> Flasks => flasks;
     public IReadOnlyList<Color> Colors => colors;
 
-    private void Awake()
+    private void Start()
     {
+        FillFlasks();
         foreach (var item in flasks)
             item.Touched.AddListener(OnFlaskTouch);
     }
@@ -47,5 +51,31 @@ public class GameManager : MonoBehaviour
             if (item.IsFullAndSameColors())
                 collected++;
         return collected == colors.Count;
+    }
+
+    private void FillFlasks()
+    {
+        List<Ball> balls = new List<Ball>();
+        for (int i = 0; i < flaskCapacity * colors.Count; i++)
+        {
+            var ball = Instantiate(prefab);
+            ball.Color = colors[i % colors.Count];
+            balls.Add(ball);
+        }
+        for (int i = 0; i < balls.Count; i++)
+        {
+            int toSwap = Random.Range(0, balls.Count);
+            var buf = balls[toSwap];
+            balls[toSwap] = balls[i];
+            balls[i] = buf;
+        }
+        Flask flask = flasks[0];
+        int next = 1;
+        for (int i = 0; i < balls.Count; i++)
+        {
+            if (flask.IsFull)
+                flask = flasks[next++];
+            flask.TryPut(balls[i]);
+        }
     }
 }
